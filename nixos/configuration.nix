f2c293bff551f0cf7af7e668b31cbb82b1a3943b
kuedan <daniel.kuecker@gmail.com>
2025-04-13 18:28:52 -0500

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -133,6 +133,7 @@ virtualisation.spiceUSBRedirection.enable = true;
     keepassxc
     libreoffice
     nextcloud-client
+    nvim
     oterm
     usbimager
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
